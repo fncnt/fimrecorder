aea948f9724f960f034ca70799d5fcd026db450d
FIM User <FIM@Uni16NB321.dom.uni-leipzig.de>
2018-09-12 08:49:23 +0200

    Partial refactoring of fimrecorder.py

--- fimrecorder.py
+++ fimrecorder.py
@@ -12,149 +12,154 @@ import settingshandler
 
 # TODO: make this a class Fim()
 # TODO: and create an object in main for readability
+# TODO: or define variables globally
 
-def main():
-    app = QApplication(sys.argv)
-    window = QMainWindow()
-    ui = ui_fimwindow.Ui_fimWindow()
-    ui.setupUi(window)
 
-    # for some reason QT Designer doesn't apply this on its own
-    # 1 = MinuteSection
-    ui.RecDurTEdit.setCurrentSectionIndex(1)
+app = QApplication(sys.argv)
+window = QMainWindow()
+ui = ui_fimwindow.Ui_fimWindow()
+camera = pyloncom.QCamera()
+disposablecam = pylonproc.QCamSnapshot()
+dcthread = QThread()
+recordingcam = pylonproc.QCamRecorder()
+recthread = QThread()
+# loads settings on __init__()
+fimsettings = settingshandler.SettingsHandler()
 
-    camera = pyloncom.QCamera()
+
+def QTimeToMsecs(time: QTime):
+    msecs = 0
+    msecs += time.msec()
+    msecs += time.second() * 1000
+    msecs += time.minute() * 1000 * 60
+    msecs += time.hour() * 1000 * 60 * 60
+    return msecs
+
+
+# Doesn't work yet
+def toggleExposureAuto(on: bool):
+    if on:
+        # Apparantly setattr() doesn't do any type conversion as = does?
+        # camera.baslerace.setCamAttr('ExposureAuto', 'ExposureAuto_Continuous')
+        camera.baslerace._cam.ExposureAuto = 'ExposureAuto_Continuous'
+        # camera.baslerace._cam.ExposureAuto.SetValue('ExposureAuto_Continuous')
+    else:
+        # camera.baslerace.setCamAttr('ExposureAuto', 'ExposureAuto_Off')
+        camera.baslerace._cam.ExposureAuto = 'ExposureAuto_Off'
+
+
+def saveSnapshot():
+    disposablecam.moveToThread(dcthread)
+
+    dcthread.started.connect(lambda: disposablecam.startProcessing(camera.frame_grabbed))
+    dcthread.start()
+    disposablecam.img_processed.connect(dcthread.quit)
+
+
+def recordVideo(toggled=bool):
+    if toggled:
+        #make sure framecount is zero so we record everything we want:
+        recordingcam.framecount = 0
+
+        recthread.start()
+        recordingcam.startProcessing(camera.frame_grabbed)
+    else:
+        recordingcam.cancelProcessing()
+        # Does terminate without blocking main thread.
+        # Also it's actually not recommended.
+        recthread.terminate()
+        # recthread.quit()
+        # recthread.wait(100)
+
+
+def pullSettings():
+    ui.ExpTimeSpinBox.setValue(fimsettings.parameters['Exposure Time'])
+    if ui.FpsEnableChkBx.isChecked():
+        ui.FpsDSpinBox.setValue(fimsettings.parameters['Frame Rate'])
+    ui.RecDurTEdit.setTime(QTime.fromString(fimsettings.parameters['Recording Duration']))
+
+    recordingcam.fcc = fimsettings.settings['Video Codec']
+    recordingcam.fpath = fimsettings.settings['Recording Directory']
+    disposablecam.fpath = fimsettings.settings['Snapshot Directory']
+    camera.baslerace.fpath = fimsettings.settings['Configuration Directory']
+    camera.baslerace.fname = fimsettings.settings['Default Camera Parameters']
+
+def pushSettings(fpath="", fname="settings.json", onlyparameters=False):
+    fimsettings.parameters['Exposure Time'] = ui.ExpTimeSpinBox.value()
+    if ui.FpsEnableChkBx.isChecked():
+        fimsettings.parameters['Frame Rate'] = ui.FpsDSpinBox.value()
+    fimsettings.parameters['Recording Duration'] = ui.RecDurTEdit.time().toString()
+    # temporaily
+    fimsettings.saveSettings(fpath, fname, onlyparameters)
+
+
+def connectSignals():
     camera.device_status[str].connect(ui.statusbar.showMessage)
     camera.device_status[str].connect(print)
+    disposablecam.status[str].connect(print)
 
-    if camera.baslerace._cam.IsOpen():
-        camera.grabInBackground()
-    #camera.is_grabbing.connect(lambda: ui.statusbar.showMessage("grabbing..."))
-
+    ui.actionSnapshot.triggered.connect(saveSnapshot)
     ui.actionRefresh.triggered.connect(camera.reset)
-    #Replace lambda by functools.partial?
+    # Replace lambda by functools.partial?
     ui.ExpTimeSpinBox.valueChanged[int].connect(
         lambda val: camera.baslerace.setCamAttr('ExposureTime', val)
     )
     ui.ExpTimeSpinBox.valueChanged[int].connect(
         lambda val: fimsettings.parameters.__setitem__('Exposure Time', val)
     )
-
-    #Doesn't work yet
-    def toggleExposureAuto(on: bool):
-        if on:
-            #Apparantly setattr() doesn't do any type conversion as = does?
-            #camera.baslerace.setCamAttr('ExposureAuto', 'ExposureAuto_Continuous')
-            camera.baslerace._cam.ExposureAuto = 'ExposureAuto_Continuous'
-            #camera.baslerace._cam.ExposureAuto.SetValue('ExposureAuto_Continuous')
-        else:
-            #camera.baslerace.setCamAttr('ExposureAuto', 'ExposureAuto_Off')
-            camera.baslerace._cam.ExposureAuto = 'ExposureAuto_Off'
+    # camera.is_grabbing.connect(lambda: ui.statusbar.showMessage("grabbing..."))
     ui.ExpAutoChkBx.toggled[bool].connect(toggleExposureAuto)
-
     ui.FpsEnableChkBx.toggled[bool].connect(
-        #lambda val: camera.baslerace.setCamAttr('AcquisitionFrameRateEnable	', int(val))
+        # lambda val: camera.baslerace.setCamAttr('AcquisitionFrameRateEnable	', int(val))
         lambda val: camera.baslerace._cam.AcquisitionFrameRateEnable.SetValue(int(val))
     )
-
     ui.FpsDSpinBox.valueChanged[float].connect(
         lambda val: camera.baslerace.setCamAttr('AcquisitionFrameRate', val)
     )
-
-    disposablecam = pylonproc.QCamSnapshot()
-    dcthread = QThread()
-    def saveSnapshot():
-        disposablecam.moveToThread(dcthread)
-
-        disposablecam.status[str].connect(print)
-        dcthread.started.connect(lambda: disposablecam.startProcessing(camera.frame_grabbed))
-        dcthread.start()
-        disposablecam.img_processed.connect(dcthread.quit)
-
-    ui.actionSnapshot.triggered.connect(saveSnapshot)
-
-    def QTimeToMsecs(time: QTime):
-        msecs = 0
-        msecs += time.msec()
-        msecs += time.second() * 1000
-        msecs += time.minute() * 1000 * 60
-        msecs += time.hour() * 1000 * 60 * 60
-        return msecs
-
-    recordingcam = pylonproc.QCamRecorder()
     recordingcam.status.connect(print)
     ui.FpsDSpinBox.valueChanged[float].connect(recordingcam.changeFps)
     ui.RecDurTEdit.timeChanged.connect(lambda val: recordingcam.msecsToFrames(QTimeToMsecs(val)))
     recordingcam.timelimit_reached.connect(ui.actionRecord.toggle)
-    recthread = QThread()
     recordingcam.moveToThread(recthread)
-
-
-    def recordVideo(toggled=bool):
-        if toggled:
-            #make sure framecount is zero so we record everything we want:
-            recordingcam.framecount = 0
-
-            recthread.start()
-            recordingcam.startProcessing(camera.frame_grabbed)
-        else:
-            recordingcam.cancelProcessing()
-            # Does terminate without blocking main thread.
-            # Also it's actually not recommended.
-            recthread.terminate()
-            #recthread.quit()
-            #recthread.wait(100)
-
     ui.actionRecord.toggled[bool].connect(recordVideo)
+    app.aboutToQuit.connect(pushSettings)
 
-    #previewthread = QThread()
-    #ui.camView.moveToThread(previewthread)
-    #previewthread.start()
 
-    #previewcam = pylonproc.QCamQPixmap()
-    #pcthread = QThread()
-    #previewcam.moveToThread(pcthread)
-    #previewcam.img_processed.connect(ui.camView.setPixmap)
-    #camera.frame_grabbed[numpy.ndarray].connect(previewcam.processImg)
-    #pcthread.started.connect(lambda: previewcam.startProcessing(camera.frame_grabbed))
-    #pcthread.start()
-    #ui.camView.setScaledContents(True)
 
-    # try loading parameters on startup on __init__()
-    fimsettings = settingshandler.SettingsHandler()
 
-    # pull Settings into program
-    def pullSettings():
-        ui.ExpTimeSpinBox.setValue(fimsettings.parameters['Exposure Time'])
-        if ui.FpsEnableChkBx.isChecked():
-            ui.FpsDSpinBox.setValue(fimsettings.parameters['Frame Rate'])
-        ui.RecDurTEdit.setTime(QTime.fromString(fimsettings.parameters['Recording Duration']))
+def main():
 
-        recordingcam.fcc = fimsettings.settings['Video Codec']
-        recordingcam.fpath = fimsettings.settings['Recording Directory']
-        disposablecam.fpath = fimsettings.settings['Snapshot Directory']
-        camera.baslerace.fpath = fimsettings.settings['Configuration Directory']
-        camera.baslerace.fname = fimsettings.settings['Default Camera Parameters']
+    ui.setupUi(window)
+    # for some reason QT Designer doesn't apply this on its own
+    # 1 = MinuteSection
+    ui.RecDurTEdit.setCurrentSectionIndex(1)
 
+    # more refined logic needed here to improve UX
+    if camera.baslerace._cam.IsOpen():
+        camera.grabInBackground()
+    # Handles interaction between UI and cam stuff
+    connectSignals()
+    # pull settings into cam classes and UI
     pullSettings()
 
-    # push Settings to SettingsHandler
-    def pushSettings():
-        fimsettings.parameters['Exposure Time'] = ui.ExpTimeSpinBox.value()
-        if ui.FpsEnableChkBx.isChecked():
-            fimsettings.parameters['Frame Rate'] = ui.FpsDSpinBox.value()
-        fimsettings.parameters['Recording Duration'] = ui.RecDurTEdit.time().toString()
+    # previewthread = QThread()
+    # ui.camView.moveToThread(previewthread)
+    # previewthread.start()
 
-        # temporaily
-        fimsettings.saveSettings()
+    # previewcam = pylonproc.QCamQPixmap()
+    # pcthread = QThread()
+    # previewcam.moveToThread(pcthread)
+    # previewcam.img_processed.connect(ui.camView.setPixmap)
+    # camera.frame_grabbed[numpy.ndarray].connect(previewcam.processImg)
+    # pcthread.started.connect(lambda: previewcam.startProcessing(camera.frame_grabbed))
+    # pcthread.start()
+    # ui.camView.setScaledContents(True)
 
-    # Save settings automatically on exit
-    # app.aboutToQuit.connect(fimsettings.saveSettings)
-    app.aboutToQuit.connect(pushSettings)
+
+
     # manually load settings via button (for reproducible measurements
     # ui.actionLoad_Parameters.connect(fimsettings.)
     # QFileDialog needed
-
     window.show()
     sys.exit(app.exec_())
 

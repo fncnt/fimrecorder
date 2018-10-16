23d8d7ff4db62e5de6e555ef67c54c2ed3c86dd0
FIM User <FIM@Uni16NB321.dom.uni-leipzig.de>
2018-10-16 11:47:55 +0200

    Attempt to increase thread priorities.
    Former-commit-id: 4c82d1c0e9664c1d320bf7f3e24d3b1710b774d5

--- pyloncom.py
+++ pyloncom.py
@@ -59,6 +59,13 @@ class QCamWorker(QObject):
 
             self.device_status.emit("Loading device configuration")
             pylon.FeaturePersistence.Load(os.path.join(self.fpath, self.fname), self._cam.GetNodeMap(), True)
+            # Attempt to ensure realtime grabbing
+            # Max == 15 for non-admin users
+            self._cam.GrabLoopThreadPriorityOverride = True
+            self._cam.InternalGrabEngineThreadPriorityOverride = True
+            self._cam.GrabLoopThreadPriority = 30
+            self._cam.InternalGrabEngineThreadPriority = 31
+            self._cam.StreamGrabber.TransferLoopThreadPriority = 32
 
         except genicam.GenericException as e:
             # Error handling.

c5abba04eebed1462111b9b2db530fba55b74385
FIM User <vincent.messow@studserv.uni-leipzig.de>
2019-03-05 12:23:03 +0100

    Fallback to hardcoded loggingconf,json path if settings.json is not present
    Former-commit-id: 0784d50d4489177c3690aa0cdb601f40347cb1c3

--- logger.py
+++ logger.py
@@ -2,10 +2,14 @@ import logging.config
 import os
 import json
 
-with open('settings.json') as tempf:
-    tempdict = json.load(tempf)
-    configdir = tempdict['Settings']['Configuration Directory']
-    configfile = tempdict['Settings']['Logging Configuration']
-    with open(os.path.join(configdir, configfile)) as f:
-        logging.config.dictConfig(json.load(f))
+try:
+    with open('settings.json') as tempf:
+        tempdict = json.load(tempf)
+        configdir = tempdict['Settings']['Configuration Directory']
+        configfile = tempdict['Settings']['Logging Configuration']
+except FileNotFoundError as e:
+    configdir = "config"
+    configfile = "loggingconf.json"
+with open(os.path.join(configdir, configfile)) as f:
+    logging.config.dictConfig(json.load(f))
 logger = logging.getLogger(__name__)

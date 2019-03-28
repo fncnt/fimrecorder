b225acc640430fcdc2b35457bc7b0fd46b6340a5
Vincent Messow <git@vncnt.eu>
2019-03-28 18:48:40 +0100

    Include version info in log files.
    Former-commit-id: f164323339695a020eda837780b5543083204fea

--- logger.py
+++ logger.py
@@ -2,6 +2,8 @@ import logging.config
 import os
 import json
 
+VERSIONCODE = "v0.3"
+
 try:
     with open('settings.json') as tempf:
         tempdict = json.load(tempf)
@@ -14,4 +16,5 @@ with open(os.path.join(configdir, configfile)) as f:
     logging.config.dictConfig(json.load(f))
 logger = logging.getLogger(__name__)
 
+logger.info("FIMrecorder version: " + VERSIONCODE)
 logger.info("Loaded logging configuration from " + os.path.join(configdir, configfile) + ".")
